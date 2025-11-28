46ece3abc825c28fcf9330c0715386239296a98e
codingpeanut <pnutz987@gmail.com>
2025-11-28 09:35:44 +0800

    modified:   scripts/app.sh

--- scripts/app.sh
+++ scripts/app.sh
@@ -24,7 +24,7 @@ baseurl=https://dl.google.com/linux/chrome/rpm/stable/x86_64
 enabled=1
 gpgcheck=1
 gpgkey=https://dl.google.com/linux/linux_signing_key.pub" | sudo tee /etc/yum.repos.d/google-chrome.repo
-    sudo dnf install --user -y google-chrome-stable
+    sudo dnf install -y google-chrome-stable
 fi
 
 # VSCode
@@ -32,7 +32,7 @@ if ! command -v code >/dev/null 2>&1; then
     echo "[*] Installing VSCode..."
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    sudo dnf install --user -y code
+    sudo dnf install -y code
 fi
 
 # CopyQ
